a8c9e6fd2d017dbffdd6baa71e0ec3506465ff64
Nontapat <thenut_project@hotmail.com>
2015-08-25 01:19:51 +0700

    570610580 three function

--- Lab/Lab1-2/Program.cs
+++ Lab/Lab1-2/Program.cs
@@ -11,30 +11,49 @@ namespace Lab1_2
         static void Main(string[] args)
         {
             bool flag = true;
-
+            int[] input = new int[10];
+            input_Num(input);
+            process(input);
+            output(input);
+            
+            Console.ReadKey();
+        }
             // input
+          
+        static void input_Num(int[] _input)
+        {
             int[] input = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
+            for (int i = 0; i != input.Length; i++)
+                _input[i] = input[i];
+        }
+        // process
+        static void process(int[] _input)
+        {
 
-            // process
+
             while (flag)
             {
                 flag = false;
-                for (int i = 0; i != input.Length - 1; i++)
-                {
-                    if (input[i] > input[i + 1])
+                
+                for (int i = 0; i != _input.Length - 1; i++)
                     {
-                        int temp = input[i];
-                        input[i] = input[i + 1];
-                        input[i + 1] = temp;
+                   
+                    if (_input[i] > _input[i + 1])
+                            {
+                        int temp = _input[i];
+                        _input[i] = _input[i + 1];
+                        _input[i + 1] = temp;
                         flag = true;
                     }
                 }
             }
-
+        }
             // output
-            for (int i = 0; i != input.Length; i++)
+            static void output(int[] _input)
+        { 
+            for (int i = 0; i != _input.Length; i++)
             {
-                Console.Write(input[i]);
+                Console.Write(_input[i]);
                 Console.Write(" ");
             }
             Console.ReadKey();
